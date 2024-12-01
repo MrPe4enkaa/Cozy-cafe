00e71fc531975665dfc946ba38975f5955b85ee9
mrpe4enushkaa_ <daniillavrika1980@gmail.com>
2024-12-01 16:08:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     let mainImg = document.getElementById('mainImg');
+//     let welcome2 = document.querySelector('.welcome2');
+//     mainImg.width = welcome2.offsetWidth;
+//     mainImg.height = welcome2.offsetWidth;
+//     console.log(welcome2.width);
+// });
